dcca0af53dc83e40ebf50d637d8fc0fb4d26655b
Lee <370528912@qq.com>
2023-02-11 03:50:53 +0800

    lab02: completed bomb

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -2,6 +2,10 @@ package bomb;
 
 import common.IntList;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class BombMain {
     public static void main(String[] args) {
         int phase = 2;
@@ -14,10 +18,22 @@ public class BombMain {
             b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(new IntList(0, new IntList(9, new IntList(3, new IntList(0, new IntList(8, null)))))); // Figure this out too
         }
+        int pass = -81201430;
+        String password = "";
+        int i = 0;
+        while (i < 1400) {
+            if (i == 1337) {
+                password += pass + " ";
+            }else {
+                password+= i + " ";
+            }
+            i++;
+        }
+
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2(password);
         }
     }
 }
