30cddd51c8afb2e8e1d8a9c1d7962eab1cc8ce06
Lee <370528912@qq.com>
2023-02-14 01:29:32 +0800

    proj1a: completed addfirst and addlast

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -11,21 +11,26 @@ public class LinkedListDeque<T> implements Deque<T> {
         private Node next;
 
         public Node() {
+            Object middleMan = new Object();
+            this.item = (T) middleMan;
+            this.prev = this;
+            this.next = this.prev;
         }
 
         public Node(T item) {
             this.item = item;
-            this.prev = (Node) this.item;
+            this.prev = this;
             this.next = this.prev;
         }
+
+
 
         /**
          * this constructor is for the list which has been already created.
          */
     }
     public LinkedListDeque() {
-        Node<T> middleMan = new Node<>();
-        this.sentinel = new Node<>((T) middleMan);
+        this.sentinel = new Node<>();
     }
 
 
@@ -36,10 +41,26 @@ public class LinkedListDeque<T> implements Deque<T> {
 
     @Override
     public void addFirst(T x) {
+        Node<T> newNode = new Node(x);
+        Node<T> servant = this.sentinel;
+        Node<T> firstNode = servant.next;
+        servant.next = newNode;
+        newNode.prev = servant;
+        firstNode.prev = newNode;
+        newNode.next = firstNode;
+
     }
 
     @Override
     public void addLast(T x) {
+        Node<T> newNode = new Node(x);
+        Node<T> servant = this.sentinel; // last node
+        Node<T> lastNode = servant.prev;
+        lastNode.next = newNode;
+        newNode.prev = lastNode;
+        servant.prev = newNode;
+        newNode.next = servant;
+
     }
 
     @Override
@@ -80,5 +101,22 @@ public class LinkedListDeque<T> implements Deque<T> {
     public static void main(String[] args) {
         LinkedListDeque<Integer> q = new LinkedListDeque<>();
         System.out.println(q.sentinel.prev.item == q.sentinel.next.item);
+        q.addLast(6);
+        q.addLast(5);
+        q.addLast(4);
+        q.addLast(3);
+        q.addLast(2);
+        q.addLast(1);
+        System.out.println("Test add first method");
+        LinkedListDeque<Integer> q2 = new LinkedListDeque<>();
+        q2.addFirst(6);
+        q2.addFirst(5);
+        q2.addFirst(4);
+        q2.addFirst(3);
+        q2.addFirst(2);
+        q2.addFirst(1);
+
+
+
     }
 }
