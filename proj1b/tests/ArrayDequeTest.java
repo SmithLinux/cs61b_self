765e81b3d1b46fb16b30e7c6e30e6850844bf35f
Lee <370528912@qq.com>
2023-02-15 08:15:40 +0800

    proj1b: completed addfirst and addlast

--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -5,4 +5,45 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
+    @Test
+    public void getLastTest(){}
+
+    @Test
+    public void getFirstTest() {}
+
+    @Test
+    public void addFirstTest() {}
+
+    @Test
+    public void addLastTest() {}
+
+    @Test
+    public void toListTest() {
+
+    }
+
+    @Test
+    public void isEmptyTest() {
+
+    }
+
+    @Test
+    public void sizeTest() {
+
+    }
+
+    @Test
+    public void getTest() {
+
+    }
+
+    @Test
+    public void removeLast() {
+
+    }
+
+    @Test
+    public void removeFirst() {
+
+    }
 }
