abebdd0f82ed684c1f0ee47cda7a443d8586165f
Lee <370528912@qq.com>
2023-02-21 08:15:04 +0800

    completed max()

--- proj1c/tests/MaxArrayDequeTest.java
+++ proj1c/tests/MaxArrayDequeTest.java
@@ -1,12 +1,37 @@
+import deque.MaxArrayDeque;
 import org.junit.jupiter.api.*;
 import deque.Deque;
 import deque.ArrayDeque;
 import deque.LinkedListDeque;
 
+import java.util.Comparator;
+
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class MaxArrayDequeTest {
+
+    private static class IntegerComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    @Test
+    public void maxTest() {
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<>(new IntegerComparator());
+        Integer expected = 9;
+        Integer maximum;
+
+        for (int i = 0; i < 10; i++) {
+            mad.addLast(i);
+        }
+
+        maximum = mad.max();
+        assertThat(maximum).isEqualTo(expected);
+    }
 
 
 
