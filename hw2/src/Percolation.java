37c9902f89e01d1b42017a66d89b76ed502f379e
Lee <370528912@qq.com>
2023-02-25 15:08:54 +0800

    start hw2

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -3,30 +3,42 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
+    WeightedQuickUnionUF grid;
 
+    // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         // TODO: Fill in this constructor.
+        grid = new WeightedQuickUnionUF(N);
+
     }
 
+    // open the site (row, col) if it is not open already
     public void open(int row, int col) {
         // TODO: Fill in this method.
+        if(!isOpen(row, col)) {
+            grid.union(row, col);
+        }
     }
 
+    // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        return grid.connected(row, col);
     }
 
+    // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         // TODO: Fill in this method.
         return false;
     }
 
+    // number of open sites
     public int numberOfOpenSites() {
         // TODO: Fill in this method.
         return 0;
     }
 
+    // does the system percolate?
     public boolean percolates() {
         // TODO: Fill in this method.
         return false;
