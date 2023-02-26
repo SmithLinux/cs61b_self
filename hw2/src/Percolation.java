58e783867c06b1f844f88485d5aa2487fcdbedaa
Lee <370528912@qq.com>
2023-02-26 12:37:11 +0800

    completed site

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,29 +1,37 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+import java.util.TreeMap;
 // TODO: Add any other necessary imports.
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
     WeightedQuickUnionUF grid;
+    boolean[] site;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         // TODO: Fill in this constructor.
-        grid = new WeightedQuickUnionUF(N);
-
+        grid = new WeightedQuickUnionUF(N * N);
+        site = new boolean[N * N];
     }
 
-    // open the site (row, col) if it is not open already
+    /**
+     *  open the site (row, col) if it is not open already.
+     */
     public void open(int row, int col) {
-        // TODO: Fill in this method.
-        if(!isOpen(row, col)) {
-            grid.union(row, col);
+        if (!isOpen(row, col)) {
+            this.site[xyTo1D(row, col)] = true;
         }
     }
 
     // is the site (row, col) open?
+    /**
+     * row 1 col 2 stand for
+     */
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return grid.connected(row, col);
+
+        return site[xyTo1D(row, col)];
     }
 
     // is the site (row, col) full?
@@ -44,7 +52,15 @@ public class Percolation {
         return false;
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
 
+    private int xyTo1D(int row, int col) {
+        int side = (int) Math.sqrt((double) this.site.length);
+        if (row == 0) { //if row is 0, then return col.
+            return col;
+        }
+        if (col == 0) {
+            return row * side;
+        }
+        return row + col * side;
+    }
 }
