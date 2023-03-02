85a86d1b68e4a732748975f1d20e2976fee31593
Lee <370528912@qq.com>
2023-03-02 23:23:12 +0800

    bug fixed

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -2,7 +2,10 @@ package aoa.guessers;
 
 import aoa.utils.FileUtils;
 import edu.princeton.cs.algs4.In;
+<<<<<<< HEAD
 import org.checkerframework.checker.units.qual.A;
+=======
+>>>>>>> 0085b4b (proj0: fig the bug of PatternAweraLetterFreqGuesser.java)
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,9 +63,14 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
      */
     public Map<Character, Integer> getFreqMapThatMatchesPattern(String pattern) {
         Map<Character, Integer> letters = new TreeMap<>();
+<<<<<<< HEAD
         List<String> matchedWords = getTheLengthMatchedWords(pattern);
         List<String> patternMatchesWords = getThePatternMatchesWords(matchedWords, pattern);
         for(String word : patternMatchesWords) {
+=======
+        List<String> matchedWords = getTheMatchedWords(pattern);
+        for(String word : matchedWords) {
+>>>>>>> 0085b4b (proj0: fig the bug of PatternAweraLetterFreqGuesser.java)
             for (int i = 0; i < word.length(); i++) {
                 if (!letters.containsKey(word.charAt(i))) {
                     letters.put(word.charAt(i), 1);
@@ -77,6 +85,7 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     /**
      * Return the list of words which matched the pattern.
      */
+<<<<<<< HEAD
     public List<String> getThePatternMatchesWords(List<String> lengthMatchesWords, String pattern) {
         List<String> patternMatchesWords = new ArrayList<>(lengthMatchesWords);
 
@@ -95,12 +104,30 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
      */
     public List<String> getTheLengthMatchedWords(String pattern) {
         List<String> sameLengthWords = new ArrayList<>();
+=======
+
+    /**
+     * Return the words which match the pattern
+     */
+    public List<String> getTheMatchedWords(String pattern) {
+        List<String> sameLengthWords = new ArrayList<>();
+        List<String> matchedWords = new ArrayList<>();
+        boolean isMatch = false;
+        int count = 0;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == '-') {
+                count ++;
+            }
+        }
+
+>>>>>>> 0085b4b (proj0: fig the bug of PatternAweraLetterFreqGuesser.java)
 
         for(String word : words) {
             if (word.length() == pattern.length()) {
                 sameLengthWords.add(word);
             }
         }
+<<<<<<< HEAD
         return sameLengthWords;
     }
 
@@ -124,5 +151,43 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
 //        System.out.println(mw2);
 //        System.out.println(pmw2);
 
+=======
+        if (count == pattern.length()) {
+            return sameLengthWords;
+        }
+
+        for(String word : sameLengthWords) {
+            for(int i = 0; i < word.length(); i ++) {
+                if(pattern.charAt(i) != '-' && pattern.charAt(i) == word.charAt(i)) {
+                    isMatch = true;
+                }else if(pattern.charAt(i) == '-' && isMatch) {
+                    isMatch = true;
+                }
+                else {
+                    isMatch = false;
+                }
+            }
+            if (isMatch) {
+                matchedWords.add(word);
+                isMatch = false;
+            }
+        }
+        return matchedWords;
+    }
+
+    public static void main(String[] args) {
+        PatternAwareLetterFreqGuesser palfg = new PatternAwareLetterFreqGuesser("data/example.txt");
+        PatternAwareLetterFreqGuesser palfg2 = new PatternAwareLetterFreqGuesser("data/sorted_scrabble.txt");
+
+        System.out.println(palfg.getGuess("-e--", List.of('e')));
+        Map<Character, Integer> lt = palfg.getFreqMapThatMatchesPattern("-e--");
+        Map<Character, Integer> lt2 = palfg.getFreqMapThatMatchesPattern("----");
+        List<String> mw = palfg.getTheMatchedWords("-e--");
+        List<String> mw2 = palfg2.getTheMatchedWords("-e-a");
+        System.out.println(lt);
+        System.out.println(lt2);
+        System.out.println(mw);
+        System.out.println(mw2);
+>>>>>>> 0085b4b (proj0: fig the bug of PatternAweraLetterFreqGuesser.java)
     }
 }
