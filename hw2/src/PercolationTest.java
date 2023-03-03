53a98aa5b5e651a7a4bf6ff5e53fe0bb7983971a
Lee <370528912@qq.com>
2023-03-03 11:28:32 +0800

    hw2 bug fixed

--- hw2/src/PercolationTest.java
+++ hw2/src/PercolationTest.java
@@ -22,13 +22,35 @@ public class PercolationTest {
     }
 
     @Test
-    public void cornerTest() {
-        Percolation p = new Percolation(6);
-        Truth.assertThat(p.isCorner(0,0)).isEqualTo(true);
-        Truth.assertThat(p.isCorner(0,5)).isEqualTo(true);
-        Truth.assertThat(p.isCorner(5,0)).isEqualTo(true);
-        Truth.assertThat(p.isCorner(5,5)).isEqualTo(true);
+    public void openTopBottomTest() {
+        Percolation p = new Percolation(5);
+        p.open(0,4);
+        p.open(1,4);
+        p.open(2,4);
+        p.open(3,4);
+
+        Truth.assertThat(p.isFull(3,4)).isEqualTo(true);
     }
+
+    @Test
+    public void openAroundTest() {
+        Percolation p = new Percolation(5);
+        p.open(0,4);
+        p.open(1,4);
+        p.open(2,4);
+        p.open(3,4);
+
+        Truth.assertThat(p.isConnected(p.xyTo1D(3,4), p.xyTo1D(2,4))).isEqualTo(true);
+    }
+
+//    @Test
+//    public void cornerTest() {
+//        Percolation p = new Percolation(6);
+//        Truth.assertThat(p.isCorner(0,0)).isEqualTo(true);
+//        Truth.assertThat(p.isCorner(0,5)).isEqualTo(true);
+//        Truth.assertThat(p.isCorner(5,0)).isEqualTo(true);
+//        Truth.assertThat(p.isCorner(5,5)).isEqualTo(true);
+//    }
 
 
     @Test
@@ -57,5 +79,30 @@ public class PercolationTest {
         p.open(7,7);
         boolean result = p.isFull(7,7);
         Truth.assertThat(result).isEqualTo(false);
+    }
+
+
+    @Test
+    public void unionAroundOpenedTest() {
+        Percolation p = new Percolation(5);
+        p.open(0, 1);
+        p.open(0,2);
+        p.open(0, 3);
+
+        Truth.assertThat(p.isConnected(p.xyTo1D(0,1), p.xyTo1D(0,2))).isEqualTo(true);
+
+        //p.unionAroundOpened(0,2);
+
+        Truth.assertThat(p.isConnected(p.xyTo1D(0,1), p.xyTo1D(0,2))).isEqualTo(true);
+    }
+
+    @Test
+    public void checkUnionTest() {
+        Percolation p = new Percolation(5);
+        p.open(0,1);
+        p.open(0,2);
+
+
+        Truth.assertThat(p.isConnected(p.xyTo1D(0,1), p.xyTo1D(0,2))).isEqualTo(true);
     }
 }
