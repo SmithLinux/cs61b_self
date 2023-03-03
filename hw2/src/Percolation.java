245eb8fc5dd7bcff2daf6e0fdb220f069c44f154
Lee <370528912@qq.com>
2023-03-03 11:28:32 +0800

    hw2 bug fixed

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,7 +1,9 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class Percolation {
@@ -9,19 +11,40 @@ public class Percolation {
     private boolean[][] site;
     private int openSites;
 
+    private Set<Integer> sitePosition;
+
+    private int top;
+    private int bottom;
+
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
-        percolate = new WeightedQuickUnionUF(N * N);
+        percolate = new WeightedQuickUnionUF(N * N + 2); // count() - 1 means top, count () means bottom
+        top = N * N;
+        bottom = N * N + 1;
         site = new boolean[N][N];
         openSites = 0;
+        sitePosition = new HashSet<>();
     }
 
     /**
-     *  open the site (row, col) if it is not open already.
+     * open the site (row, col) if it is not open already.
      */
     public void open(int row, int col) {
         if (!isOpen(row, col)) {
+            if (row == 0) {
+                percolate.union(this.top, xyTo1D(row, col));
+            }
+            if (row == this.site[0].length - 1 && !percolates()) {
+                percolate.union(this.bottom, xyTo1D(row, col));
+            }
             this.site[row][col] = true;
+
+
+            if (!this.sitePosition.contains(xyTo1D(row, col))) {
+                sitePosition.add(xyTo1D(row, col));
+            }
+
+            this.unionAroundOpened(row, col);
             openSites++;
         }
     }
@@ -38,7 +61,7 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         // if this sites is connected to the top row which is opened, then return true
-        return false;
+        return this.isConnected(this.xyTo1D(row, col), top);
     }
 
     // number of open sites
@@ -51,11 +74,11 @@ public class Percolation {
      */
     // does the system percolate?
     public boolean percolates() {
-        return false;
+        return this.isConnected(top, bottom);
     }
 
     public int xyTo1D(int row, int col) {
-        int side = (int) Math.sqrt((double) this.site.length);
+        int side = this.site[0].length;
         if (row == 0) { //if row is 0, then return col.
             return col;
         }
@@ -66,14 +89,38 @@ public class Percolation {
     }
 
 
-    public boolean isCorner(int row, int col) {
-        if (row == 0 & (col == 0 || this.site[0].length - 1 == col)) {
-            return true;
-        }
-        if (row == this.site[0].length - 1 && (col == this.site[0].length - 1 || col == 0)) {
-            return true;
-        }
+    public void unionAroundOpened(int row, int col) {
+        int pos = xyTo1D(row, col);
+        int upperPos = xyTo1D(row + 1, col);
+        int lowerPos = xyTo1D(row - 1, col);
+        int leftPos = xyTo1D(row, col - 1);
+        int rightPos = xyTo1D(row, col + 1);
 
-        return false;
+
+        checkUnion(pos, upperPos);
+        checkUnion(pos, lowerPos);
+        checkUnion(pos, leftPos);
+        checkUnion(pos, rightPos);
+    }
+
+
+    public void checkUnion(int pos, int otherPos) {
+        if (this.sitePosition.contains(otherPos)) {
+            if (!this.isConnected(pos, otherPos)) {
+                this.connectTwoSites(pos, otherPos);
+            }
+        }
+    }
+
+    public boolean isConnected(int pos, int otherPos) {
+        return this.percolate.connected(pos, otherPos);
+    }
+
+    public void connectTwoSites(int pos, int otherPos) {
+        this.percolate.union(pos, otherPos);
+    }
+
+    private int fullPos() {
+        return top;
     }
 }
