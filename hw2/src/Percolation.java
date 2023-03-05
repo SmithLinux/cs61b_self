8b094498d4cb63a7aa2cc57baf00b0f44d5a1113
Lee <370528912@qq.com>
2023-03-05 23:54:55 +0800

    lab07:fix bugs

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -6,24 +6,39 @@ import java.util.List;
 import java.util.Set;
 
 
+/**
+ * all methods should take constant time plus a constant number of calls to the union-find methods union(), find(), connected(), and count()
+ */
 public class Percolation {
     private WeightedQuickUnionUF percolate;
     private boolean[][] site;
     private int openSites;
-
     private Set<Integer> sitePosition;
+    private int virtualTop;
+    private int virtualBottom;
 
-    private int top;
-    private int bottom;
+    private WeightedQuickUnionUF mirror;
+    private int mirrorTop;
+
 
     // create N-by-N grid, with all sites initially blocked
+    /**
+     * The constructor should take time proportional to N^2
+     */
     public Percolation(int N) {
-        percolate = new WeightedQuickUnionUF(N * N + 2); // count() - 1 means top, count () means bottom
-        top = N * N;
-        bottom = N * N + 1;
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        percolate = new WeightedQuickUnionUF(N * N + 2);
+        mirror = new WeightedQuickUnionUF(N * N + 1);
+        mirrorTop = mirror.count() - 1;
+        // count() - 2 means top, count () - 1 means bottom
+        virtualTop = percolate.count() - 2;
+        virtualBottom = percolate.count() - 1;
         site = new boolean[N][N];
         openSites = 0;
         sitePosition = new HashSet<>();
+
     }
 
     /**
@@ -32,24 +47,28 @@ public class Percolation {
     public void open(int row, int col) {
         if (!isOpen(row, col)) {
             if (row == 0) {
-                percolate.union(this.top, xyTo1D(row, col));
+                percolate.union(this.virtualTop, xyTo1D(row, col));
+                mirror.union(this.mirrorTop, xyTo1D(row, col));
             }
             if (row == this.site[0].length - 1 && !percolates()) {
-                percolate.union(this.bottom, xyTo1D(row, col));
-            }
-            this.site[row][col] = true;
+                percolate.union(xyTo1D(row, col), this.virtualBottom);
 
-
-            if (!this.sitePosition.contains(xyTo1D(row, col))) {
+            } else if (!this.sitePosition.contains(xyTo1D(row, col))) {
                 sitePosition.add(xyTo1D(row, col));
             }
 
+            this.site[row][col] = true;
+
+
+
             this.unionAroundOpened(row, col);
             openSites++;
+
         }
     }
 
-    // is the site (row, col) open?
+
+    // is the site (row, col) open? Completed
     public boolean isOpen(int row, int col) {
 
         return site[row][col];
@@ -61,10 +80,10 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         // if this sites is connected to the top row which is opened, then return true
-        return this.isConnected(this.xyTo1D(row, col), top);
+        return this.mirror.connected(this.xyTo1D(row, col), mirrorTop);
     }
 
-    // number of open sites
+    // number of open sites Completed
     public int numberOfOpenSites() {
         return openSites;
     }
@@ -74,17 +93,15 @@ public class Percolation {
      */
     // does the system percolate?
     public boolean percolates() {
-        return this.isConnected(top, bottom);
+        return this.percolate.connected(virtualBottom, virtualTop);
     }
 
     public int xyTo1D(int row, int col) {
         int side = this.site[0].length;
-        if (row == 0) { //if row is 0, then return col.
-            return col;
+        if (row < 0 || row >= side || col < 0 || col >= side) {
+            return -1;
         }
-        if (col == 0) {
-            return row * side;
-        }
+
         return row * side + col;
     }
 
@@ -106,21 +123,16 @@ public class Percolation {
 
     public void checkUnion(int pos, int otherPos) {
         if (this.sitePosition.contains(otherPos)) {
-            if (!this.isConnected(pos, otherPos)) {
+            if (!this.percolate.connected(pos, otherPos)) {
                 this.connectTwoSites(pos, otherPos);
             }
         }
     }
 
-    public boolean isConnected(int pos, int otherPos) {
-        return this.percolate.connected(pos, otherPos);
-    }
 
     public void connectTwoSites(int pos, int otherPos) {
         this.percolate.union(pos, otherPos);
+        this.mirror.union(pos, otherPos);
     }
 
-    private int fullPos() {
-        return top;
-    }
 }
---
+++ lab07/src/BSTMap.java
@@ -0,0 +1,151 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * Invariants
+ * 1. All leaves are the same distance from the root.
+ * 2, A non-leaf node with k items must have exactly k + 1 children.
+ */
+public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
+
+
+    private Node root;
+    private int size;
+
+    @Override
+    public void put(K key, V value) {
+        boolean isReplaced = false;
+        if (root == null) {
+            this.size++;
+            this.root = new Node(key, value);
+        } else {
+            isReplaced = root.insert(root, key, value);
+        }
+        if (isReplaced) {
+            this.size++;
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        if (root == null) {
+            return null;
+        }
+        return root.getValue(key);
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (this.root == null) {
+            return false;
+        }
+        return this.root.contain(key);
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /** Removes every mapping from this map. */
+    @Override
+    public void clear() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    private class Node {
+        K key;
+        V value;
+        boolean leaf;
+        Node left;
+        Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.leaf = true;
+        }
+
+        public boolean isLeaf() {
+            return leaf;
+        }
+
+        public boolean insert(Node node, K key, V value) {
+            if (node.key.compareTo(key) == 0) {
+                node.value = value;
+                return false;
+            }
+            if(node.key.compareTo(key) > 0) {
+                if (node.left == null) {
+                    node.left = new Node(key, value);
+                    node.leaf = false;
+                }
+                return insert(node.left, key, value); // insert to the left leaf
+            } else if (node.key.compareTo(key) < 0) {   // insert to the right leaf
+                if (node.right == null) {
+                    node.right = new Node(key, value);
+                    node.leaf = false;
+                }
+                return insert(node.right, key, value);
+            } else {
+            }
+        }
+
+        public boolean contain(K key) {
+            if (this.isLeaf()) {
+                return this.key.compareTo(key) == 0;
+            }
+            if (this.key.compareTo(key) == 0) {
+                return true;
+            }
+            if (this.key.compareTo(key) > 0) {
+                return this.left.contain(key);
+            } else {
+                return this.right.contain(key);
+            }
+        }
+
+        public V getValue(K key) {
+            if (this.key.compareTo(key) == 0) {
+                return this.value;
+            }
+            if (this.isLeaf()) {
+                return null;
+            }
+            if (this.key.compareTo(key) > 0) {
+                return this.left.getValue(key);
+            } else if (this.key.compareTo(key) < 0) {
+                return this.right.getValue(key);
+            }
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi2".compareTo("hi10"));
+    }
+
+
+
+}
